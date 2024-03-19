5456dccd8d894608dbb4af4bf9b893706e84abc8
Wojtek <wojtek.plciennik@gmail.com>
2024-03-19 04:05:13 -0700

    declare Student class

---
+++ Student.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Person.hpp"
+
+class Student : public Person {
+private:
+    size_t indexNumber_{0};
+
+public:
+    Student() = default;
+    Student(std::string name, std::string surname, std::string address, std::string pesel, std::string gender, size_t indexNumber);
+    ~Student() = default;
+    void setIndex(const size_t);
+    
+    size_t getIndex() const;
+
+    void display() const override;
+};
