771c0bc049a8ac8c853ef119c42f2cc213d690da
Wojtek <wojtek.plciennik@gmail.com>
2024-03-19 07:57:38 -0700

    implementing sorting functions

--- University.cpp
+++ University.cpp
@@ -29,3 +29,47 @@ void University::addEmployee(std::string name, std::string surname, std::string 
         university_.emplace_back(std::make_unique<Employee>(name, surname, address, pesel,gender, salary));
     }
 }
+
+Person* University::findBySurname(const std::string& surname) {
+    auto isTheSame = [&surname](std::unique_ptr<Person>& person) {
+        return person->getSurname() == surname;
+    };
+    auto it = std::find_if(university_.begin(), university_.end(), isTheSame);
+
+    if (it == university_.end()) {
+        return nullptr;
+    }
+
+    Person* person_ptr = it->get();
+
+    return person_ptr;
+}
+
+Person* University::findByPesel(const std::string& pesel) {
+    auto isTheSame = [&pesel](std::unique_ptr<Person>& person) {
+        return person->getPesel();
+    };
+
+    auto it = std::find_if(university_.begin(), university_.end(), isTheSame);
+
+    if (it == university_.end()) {
+        return nullptr;
+    }
+
+    Person* person_ptr = it->get();
+    return person_ptr;
+}
+
+void University::sortByPesel() {
+    std::sort(university_.begin(), university_.end(),
+                [](const std::unique_ptr<Person>& lhs, const std::unique_ptr<Person>& rhs) {
+                    return lhs->getPesel() > rhs->getPesel();
+                });
+}
+
+void University::sortBySurname() {
+    std::sort(university_.begin(), university_.end(),
+                [](const std::unique_ptr<Person>& lhs, const std::unique_ptr<Person>& rhs) {
+                    return lhs->getSurname() < rhs->getSurname();
+                })
+}
