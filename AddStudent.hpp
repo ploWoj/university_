fd37f258b35ce7ec819561a7c8c8ccdb5f367d5a
Wojtek <wojtek.plciennik@gmail.com>
2024-03-19 05:43:37 -0700

    Implementing AddStudent command

---
+++ AddStudent.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <array>
+#include <iostream>
+#include <studio.h>
+#include <string>
+
+#include "Command.hpp"
+
+class AddStudent : public Command {
+public:
+    AddStudent() = default;
+
+    void operator()(University& university) override {
+        this->getName();
+        std::cout << "......................................................................................\n";
+        std::array<std::string, 6> data{};
+        std::array<std::string, 6> data_type{"Name: ", "Surname: ", "Address: ", "Pesel: ", "Gender: ", "IndexNumber: " };
+        std::string str;
+
+        for (size_t i = 0; i < data.size(); ++i) {
+            std::cout << data_type[i] << '\n';
+            std::cin >> str;
+            data[i] = str;
+            str = "";
+        }
+
+        university.addStudent(data[0],data[1],data[2],data[3],data[4],std::stod(data[5]));
+        std::cout << "......................................................................................\n";
+        std::cout << "Done\n";
+        getchar();
+    }
+
+    void getName() {
+        std::cout << "AddStudent\n";
+    }
+}
