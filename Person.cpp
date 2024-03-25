918cbcd2b88a7bda595aad60dd1b65de65f0884f
Wojtek <wojtek.plciennik@gmail.com>
2024-03-25 05:54:03 -0700

    auto format Person class

--- Person.cpp
+++ Person.cpp
@@ -1,9 +1,12 @@
 #include "Person.hpp"
 
-std::string getStringToChar(std::istream& in, char delimiter) {
+std::string getStringToChar(std::istream &in, char delimiter)
+{
     std::string temp{};
-    for (char current_char{}; in >> current_char;) {
-        if (current_char == delimiter) {
+    for (char current_char{}; in >> current_char;)
+    {
+        if (current_char == delimiter)
+        {
             in.putback(current_char);
             break;
         }
@@ -13,83 +16,98 @@ std::string getStringToChar(std::istream& in, char delimiter) {
     return temp;
 }
 
-Person::Person(const std::string& name, 
-               const std::string& surname, 
-               const std::string& address, 
-               const std::string& pesel, 
-               const std::string& gender)
-        : name_(name), 
-          surname_(surname), 
-          address_(address), 
-          pesel_(pesel), 
-          gender_(gender) {
-            if (!Pesel::validatePesel(pesel_)) {
-                throw std::runtime_error("not a valid pesel");
-            }
-          }
+Person::Person(const std::string &name,
+               const std::string &surname,
+               const std::string &address,
+               const std::string &pesel,
+               const std::string &gender)
+    : name_(name),
+      surname_(surname),
+      address_(address),
+      pesel_(pesel),
+      gender_(gender)
+{
+    if (!Pesel::validatePesel(pesel_))
+    {
+        throw std::runtime_error("not a valid pesel");
+    }
+}
 
-Person::Person(std::string&& name, 
-               std::string&& surname, 
-               std::string&& address, 
-               std::string&& pesel, 
-               std::string&& gender)
-        :name_(std::move(name)),
-         surname_(std::move(surname)), 
-         address_(std::move(address)), 
-         pesel_(std::move(pesel)), 
-         gender_(std::move(gender)) {
-            if (!Pesel::validatePesel(pesel_)) {
-                throw std::runtime_error("not a valid pesel");
-            }
-         } 
+Person::Person(std::string &&name,
+               std::string &&surname,
+               std::string &&address,
+               std::string &&pesel,
+               std::string &&gender)
+    : name_(std::move(name)),
+      surname_(std::move(surname)),
+      address_(std::move(address)),
+      pesel_(std::move(pesel)),
+      gender_(std::move(gender))
+{
+    if (!Pesel::validatePesel(pesel_))
+    {
+        throw std::runtime_error("not a valid pesel");
+    }
+}
 
-void Person::setName(const std::string& name) {
+void Person::setName(const std::string &name)
+{
     name_ = name;
 }
 
-void Person::setSurname(const std::string& surname) {
+void Person::setSurname(const std::string &surname)
+{
     surname_ = surname;
 }
 
-void Person::setAddress(const std::string& address) {
+void Person::setAddress(const std::string &address)
+{
     address_ = address;
 }
 
-void Person::setPesel(const std::string& pesel) {
+void Person::setPesel(const std::string &pesel)
+{
     pesel_ = pesel;
 }
 
-void Person::setGender(const std::string& gender) {
+void Person::setGender(const std::string &gender)
+{
     gender_ = gender;
 }
 
-std::string Person::getName() const { 
+std::string Person::getName() const
+{
     return name_;
 }
 
-std::string Person::getSurname() const {
+std::string Person::getSurname() const
+{
     return surname_;
 }
 
-std::string Person::getAddress() const {
+std::string Person::getAddress() const
+{
     return address_;
 }
 
-std::string Person::getPesel() const {
+std::string Person::getPesel() const
+{
     return pesel_;
 }
 
-std::string Person::getGender() const {
+std::string Person::getGender() const
+{
     return gender_;
 }
 
-std::ostream& operator<<(std::ostream& os, const Person& person) {
-    os << person.name_  << person.surname_  << person.address_ <<
-         person.gender_ ;
+std::ostream &operator<<(std::ostream &os, const Person &person)
+{
+    os << person.name_ << person.surname_ << person.address_ << person.gender_;
     return os;
 }
 
- std::istream& operator>>(std::istream& in, Person& person) {
+std::istream &operator>>(std::istream &in, Person &person)
+{
     char delim1{}, delim2{}, delim3{}, delim4{};
     person.name_ = getStringToChar(in, Person::formatDelimiter);
     in >> delim1;
@@ -104,21 +122,24 @@ std::ostream& operator<<(std::ostream& os, const Person& person) {
     if (delim1 != Person::formatDelimiter ||
         delim2 != Person::formatDelimiter ||
         delim3 != Person::formatDelimiter ||
-        delim4 != Person::formatDelimiter ) {
-            in.clear(std::ios_base::failbit);
-        }
+        delim4 != Person::formatDelimiter)
+    {
+        in.clear(std::ios_base::failbit);
+    }
     return in;
- }
+}
 
- bool Person::operator==(const Person& person) const {
-            return this->name_ == person.name_ && 
-                   this->surname_ == person.surname_ && 
-                   this->address_ == person.address_ &&
-                   this->pesel_ == person.pesel_ &&
-                   this->gender_ == person.gender_;
-        }
+bool Person::operator==(const Person &person) const
+{
+    return this->name_ == person.name_ &&
+           this->surname_ == person.surname_ &&
+           this->address_ == person.address_ &&
+           this->pesel_ == person.pesel_ &&
+           this->gender_ == person.gender_;
+}
 
-bool Pesel::validatePesel(const std::string& pesel) {
+bool Pesel::validatePesel(const std::string &pesel)
+{
     if (pesel.size() != Pesel::peselSize)
     {
         return false;
--- Person.hpp
+++ Person.hpp
@@ -5,58 +5,61 @@
 #include <memory>
 #include <string>
 
-namespace Pesel {
+namespace Pesel
+{
     constexpr auto peselSize = 11u;
-    bool validatePesel(const std::string&);
+    bool validatePesel(const std::string &);
 }
 
-class Person {
-    protected:
-        std::string name_ = "";       
-        std::string surname_ = "";
-        std::string address_ = "";
-        std::string pesel_ = "";
-        std::string gender_ = "";
-    public:
-        static constexpr char formatDelimiter{','};
+class Person
+{
+protected:
+    std::string name_ = "";
+    std::string surname_ = "";
+    std::string address_ = "";
+    std::string pesel_ = "";
+    std::string gender_ = "";
 
-        Person() = default;
-        Person(const std::string& name, 
-               const std::string& surname, 
-               const std::string& address, 
-               const std::string& pesel, 
-               const std::string& gender);
+public:
+    static constexpr char formatDelimiter{','};
 
-        Person(std::string&& name, 
-               std::string&& surname, 
-               std::string&& address, 
-               std::string&& pesel, 
-               std::string&& gender);
+    Person() = default;
+    Person(const std::string &name,
+           const std::string &surname,
+           const std::string &address,
+           const std::string &pesel,
+           const std::string &gender);
 
-        Person(const Person&) = default;
-        Person(Person&&) = default;
+    Person(std::string &&name,
+           std::string &&surname,
+           std::string &&address,
+           std::string &&pesel,
+           std::string &&gender);
 
-        Person& operator=(const Person&) = default;
-        Person& operator=(Person&&) = default;
-        
-        virtual ~Person() = default;
+    Person(const Person &) = default;
+    Person(Person &&) = default;
 
-        void setName(const std::string&);
-        void setSurname(const std::string&);
-        void setAddress(const std::string&);
-        void setPesel(const std::string&);
-        void setGender(const std::string&);
+    Person &operator=(const Person &) = default;
+    Person &operator=(Person &&) = default;
 
-        [[nodiscard]] std::string getName() const;
-        [[nodiscard]] std::string getSurname() const;
-        [[nodiscard]] std::string getAddress() const;
-        [[nodiscard]] std::string getPesel() const;
-        [[nodiscard]] std::string getGender() const;
+    virtual ~Person() = default;
 
-        friend std::ostream& operator<<(std::ostream& os, const Person&);
-        friend std::istream& operator>>(std::istream& is, Person&);
+    void setName(const std::string &);
+    void setSurname(const std::string &);
+    void setAddress(const std::string &);
+    void setPesel(const std::string &);
+    void setGender(const std::string &);
 
-        bool operator==(const Person& person) const;
+    [[nodiscard]] std::string getName() const;
+    [[nodiscard]] std::string getSurname() const;
+    [[nodiscard]] std::string getAddress() const;
+    [[nodiscard]] std::string getPesel() const;
+    [[nodiscard]] std::string getGender() const;
+
+    friend std::ostream &operator<<(std::ostream &os, const Person &);
+    friend std::istream &operator>>(std::istream &is, Person &);
+
+    bool operator==(const Person &person) const;
 };
 
-std::string getStringToChar(std::istream&, char delimeter);
+std::string getStringToChar(std::istream &, char delimeter);
