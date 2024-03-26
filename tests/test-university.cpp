2a0fc8e7253e5f40a7c93da9fbd6dfb502f0ff22
Wojtek <wojtek.plciennik@gmail.com>
2024-03-26 11:03:24 -0700

    test find by pesel

--- tests/test-university.cpp
+++ tests/test-university.cpp
@@ -5,53 +5,84 @@
 #include <assert.h>
 
 SCENARIO("Should add Students and Employees to the base and display the list",
-         "[universitybase][add][display]") {
-    GIVEN("A UniversityBase object, a Student and an Employee") {
+         "[universitybase][add][display]")
+{
+    GIVEN("A UniversityBase object, a Student and an Employee")
+    {
         University base{};
-        Student student{"Jan",        "Kowalski",
-                        "Warszawa",   "80102818499",
-                       "male", 12345};
+        Student student{"Jan", "Kowalski",
+                        "Warszawa", "80102818499",
+                        "male", 12345};
 
         Employee employee{
-            "Anna",         "Nowak",
-            "Krakow",       "92092312978",
+            "Anna", "Nowak",
+            "Krakow", "92092312978",
             "female", 7777};
 
         static std::string str{
             "Employees:\nAnna,Nowak,Krakow,92092312978,female,7777\n"
             "Students:\nJan,Kowalski,Warszawa,80102818499,male,12345\n"};
 
-        WHEN("They are added to the base and displayed to a stream") {
+        WHEN("They are added to the base and displayed to a stream")
+        {
             base.addStudent(student);
             base.addEmployee(employee);
 
             std::stringstream stream{};
             base.displayBase(stream);
 
-            THEN("The obtained string should match the str string") {
+            THEN("The obtained string should match the str string")
+            {
                 REQUIRE(stream.str() == str);
             }
         }
     }
 }
 
-SCENARIO("Should search by surname string",
-        "[universitybase][search][surname]") {
-    GIVEN("A student added to university database object") {
+SCENARIO("Should find by surname string",
+         "[universitybase][find][surname]")
+{
+    GIVEN("A student added to university database object")
+    {
         University base{};
-        Student student{"Jan",        "Kowalski",
-                        "Warszawa",   "80102818499",
-                       "male", 12345};
-        std::unique_ptr<Student> studentPtr = std::make_unique<Student>(student);
+        Student student{"Jan", "Kowalski",
+                        "Warszawa", "80102818499",
+                        "male", 12345};
         
         base.addStudent(student);
-    WHEN("search_by_surname is called") {
-        auto person_ptr = base.findBySurname(student.getPesel());
-    THEN("Should return pointet with student data") {
-        auto student_ptr = dynamic_cast<Student*>(person_ptr);
-        REQUIRE(student_ptr);
-        REQUIRE(*student_ptr == *studentPtr.get());
-    }
-    }
-    }
+        WHEN("findBySurname is called")
+        {
+            auto person_ptr = base.findBySurname(student.getPesel());
+            THEN("Should return pointet with student data")
+            {
+                auto student_ptr = dynamic_cast<Student*>(person_ptr);
+                REQUIRE(student_ptr);
+                REQUIRE(*student_ptr == student);
+            }
         }
+    }
+}
+
+SCENARIO("Should search by pesel string",
+         "[universitybase][search][pesel]")
+{
+    GIVEN("A student added to university database object")
+    {
+        University base{};
+        Student student{"Jan", "Kowalski",
+                        "Warszawa", "80102818499",
+                        "male", 12345};
+        
+        base.addStudent(student);
+        WHEN("findByPesel is called")
+        {
+            auto person_ptr = base.findBySurname(student.getPesel());
+            THEN("Should return pointet with student data")
+            {
+                auto student_ptr = dynamic_cast<Student*>(person_ptr);
+                REQUIRE(student_ptr);
+                REQUIRE(*student_ptr == student);
+            }
+        }
+    }
+}
